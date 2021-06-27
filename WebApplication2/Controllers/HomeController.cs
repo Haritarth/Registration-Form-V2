44a696df895bf007cd15bb3d815d6d8988f3625f
Haritarth <haritarth@gmail.com>
2021-06-28 01:59:01 +0530

    First Commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home  
+        public ActionResult Index()
+        {
+            Employee emp = new Employee();
+            return View(emp);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]  
+        public ActionResult SubmitEmp(Employee emp)
+        {
+            TempData["Emp"] = emp;
+
+            if (ModelState.IsValid)
+            {
+                return Redirect("/Home/EmpDetails");
+
+            }
+            Console.Write("Errorrrrrrr");
+            return View(emp);
+
+        }
+
+        public ActionResult EmpDetails()
+        {
+            Employee emp = TempData["Emp"] as Employee;
+           return View(emp);
+        }
+    }
+}
---
+++ WebApplication2/Models/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Employee
+    {
+        [Required]
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public string MiddleName
+        {
+            get;
+            set;
+        }
+        public string TelephoneNumber
+        {
+            get;
+            set;
+        }
+
+        //[Required]
+        [EmailAddress]
+        [Display(Name = "EmailAddress")]
+        public string EmailAddress 
+        { 
+            get; 
+            set; 
+        }
+
+        public string IdentificationNumber
+        {
+            get;
+            set;
+        }
+        public DateTime DateOfBirth
+        {
+            get;
+            set;
+        }
+        public string StreetAddress
+        {
+            get;
+            set;
+        }
+        public string SecondaryAddress
+        {
+            get;
+            set;
+        }
+        public string City
+        {
+            get;
+            set;
+        }
+        public string State
+        {
+            get;
+            set;
+        }
+        public string Zip
+        {
+            get;
+            set;
+        }
+
+    }
+}
